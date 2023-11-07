2aa96545492e023e7f921f12ab9e7598d35ff2a5
Maverick Grayer <maverick72032@gmail.com>
2023-11-07 15:14:45 -0500

    Exercise 2 complete

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject? (ex. English, Math, etc)");
+            string favSubject = Console.ReadLine();
+
+            switch (favSubject)
+            {
+                case "Math":
+                    Console.WriteLine("Math is my second favorite!");
+                    break;
+                case "English":
+                    Console.WriteLine("Ugh, noooooooo...");
+                    break;
+                case "History":
+                    Console.WriteLine("Oh, history is kinda fun to learn.");
+                    break;
+                case "Science":
+                    Console.WriteLine("That's my favorite!");
+                    break;
+                case "Biology":
+                    Console.WriteLine("Interesting, but difficult.");
+                    break;
+                default:
+                    Console.WriteLine("That's a real nice subject. Yeah, um, nothin' beats that, um, subject.");
+                    break;
+
+            }
         }
     }
 }
